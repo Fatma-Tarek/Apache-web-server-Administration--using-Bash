74126a68fa5d55217d594c3d812a203d61d05c7f
Fatma <fatma.tarek204@gmail.com>
2021-03-13 16:15:48 +0200

    removed unnecessary files

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $(/etc/init.d/apache2 status | grep -v grep | grep 'Apache2 is running' | wc -l) > 0 ]
-then
- echo "Process is running."
-else
-  echo "Process is not running."
-fi
